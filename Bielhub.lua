d513703402e78d88e61fc81e102c80e96da5e217
Tz07 <fxkvzin@gmail.com>
2024-12-11 15:31:39 -0300

    Update Bielhub.lua

--- Bielhub.lua
+++ Bielhub.lua
@@ -2656,7 +2656,7 @@ ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable = true
-ImageButton.Image = "http://www.roblox.com/asset/?id=109212603905955"
+ImageButton.Image = "http://www.roblox.com/asset/?id=8964489645"
 ImageButton.MouseButton1Down:connect(function()
     game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.End,false,game)
 end)
