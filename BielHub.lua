b86fa5ba85c46d8dc127b6f4a473ad823232e969
Tz07 <fxkvzin@gmail.com>
2024-12-17 18:01:44 -0300

    Update BielHub vesionV3.lua

--- BielHub.lua
+++ BielHub.lua
@@ -9,8 +9,7 @@ local function createWindow()
         ConfigFolder = "OrionTest",
         IntroText = "Bem-vindo ao BS HUB",
         Theme = {
-        BackgroundColor = Color3.fromRGB(255, 0, 0),
-        }
+        BackgroundColor = Color3.fromRGB(255,255, 255)
     })
 
     -- Main Farming
@@ -28,6 +27,14 @@ local function createWindow()
         end    
     })    
     
+    -- ConfigFarm
+    local ConfigTab = Window:MakeTab({
+        Name = "ConfigFarm",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+    
+        
     MainFarmingTab:AddDropdown({
         Name = "Select Attack Type",
         Default = "1",
@@ -44,27 +51,6 @@ local function createWindow()
             print("Fast attack on")
             -- Adicione aqui a função que você deseja executar
         end    
-    })
-    
-local ScreenGui = Instance.new("ScreenGui")
-local TextLabel = Instance.new("TextLabel")
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-TextLabel.Parent = ScreenGui
-TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-TextLabel.Size = UDim2.new(0, 200, 0, 50)
-TextLabel.Position = UDim2.new(0.5, -100, 0.4, -25) -- Ajuste a posição conforme necessário
-TextLabel.Text = "Farm Settings Boss"
-TextLabel.TextColor3 = Color3.new(0, 0, 0) -- Cor do texto
-TextLabel.TextScaled = true -- Ajusta o tamanho do texto automaticamente
-TextLabel.BackgroundTransparency = 1 -- Torna o fundo transparente
-
-    -- SubFarming
-    local SubFarmingTab = Window:MakeTab({
-        Name = "SubFarming",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
     })
 
     -- Stats
