ad95efb7995a6f24d4d708493c9db501fe43c5df
Tz07 <fxkvzin@gmail.com>
2024-12-17 16:18:09 -0300

    Update BielHub.lua

--- BielHub.lua
+++ BielHub.lua
@@ -1,78 +1,138 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "BS HUB", SubTitle = "discord.gg/bielscript",HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window
 
--- Main Farming
-local MainFarmingTab = Window:MakeTab({
-    Name = "Main Farming",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+local function createWindow()
+    Window = OrionLib:MakeWindow({
+        Name = "BS HUB",
+        SubTitle = "discord.gg/bielscript",
+        HidePremium = false,
+        SaveConfig = true,
+        ConfigFolder = "OrionTest",
+        IntroText = "Bem-vindo ao BS HUB",
+        Theme = {
+            MainColor = Color3.fromRGB(255, 0, 0),
+            AccentColor = Color3.fromRGB(255, 0, 0),
+            BackgroundColor = Color3.fromRGB(30, 30, 30),
+            BorderColor = Color3.fromRGB(255, 0, 0)
+        }
+    })
 
-MainFarmingTab:AddButton({
-    Name = "Auto farm level",
-    Callback = function()
-        print("Farming iniciado!")
-        -- Adicione aqui a função que você deseja executar
-    end    
-})
-MainFarmingTab:AddButton({
-    Name = "Fast attack",
-    Callback = function()
-        print("Fast attack on")
-        -- Adicione aqui a função que você deseja executar
-    end    
-})
+    -- Main Farming
+    local MainFarmingTab = Window:MakeTab({
+        Name = "Main Farming",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 
--- SubFarming
-local SubFarmingTab = Window:MakeTab({
-    Name = "SubFarming",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+    MainFarmingTab:AddButton({
+        Name = "Auto farm level",
+        Callback = function()
+            print("Farming iniciado!")
+            -- Adicione aqui a função que você deseja executar
+        end    
+    })
 
--- Stats
-local StatsTab = Window:MakeTab({
-    Name = "Stats",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+    MainFarmingTab:AddButton({
+        Name = "Fast attack",
+        Callback = function()
+            print("Fast attack on")
+            -- Adicione aqui a função que você deseja executar
+        end    
+    })
+    
+    MainFarmingTab:AddDropdown({
+        Name = "Select Attack Type",
+        Default = "1",
+        Options = {"Normal Attack", "Fast attack", "Super Fas Attack"},
+        Callback = function(selected)
+            print("Você selecionou: " .. selected)
+            -- Adicione aqui a função que você deseja executar com base na seleção
+        end    
+    })
+    
+    MainFarmingTab:AddDropdown({
+        Name = "Sea 1 Select boss",
+        Default = "1",
+        Options = {"Vice Admiral", "Gorilla King", "Bobby", "The Saw", "Yeti", "Mob Leader", "Saber Expert", "Warden", "Chief Warden", "Swan", "Magma Admiral", "Fishman Lord", "Wysper", "Thunder God", "Cyborg", "Ice Admiral", "GreyBeard"},
+        Callback = function(selected)
+            print("Você selecionou: " .. selected)
+            -- Adicione aqui a função que você deseja executar com base na seleção
+        end    
+    })
 
--- Frutas
-local FrutasTab = Window:MakeTab({
-    Name = "Frutas",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+MainFarmingTab:AddDropdown({
+        Name = "Sea 2 Select Boss",
+        Default = "1",
+        Options = {"Diamont", "Jeremy", "Fajita", "Don Swan", "Smoke Admiral", "Awakened Ice Admiral", "Tide Keeper", "Dark Beard", "Cursed Captain", "Order"},
+        Callback = function(selected)
+            print("Você selecionou: " .. selected)
+            -- Adicione aqui a função que você deseja executar com base na seleção
+        end    
+    })
+    
+    MainFarmingTab:AddDropdown({
+        Name = "Sea 3 Select Boss",
+        Default = "1",
+        Options = {"Stone", "Island Empress", "Kilo Admiral", "Captain Elephant", "Beautiful Pirate", "Cake queen", "Longma", "Soul Reaper", "Rip_Indra True Form"},
+        Callback = function(selected)
+            print("Você selecionou: " .. selected)
+            -- Adicione aqui a função que você deseja executar com base na seleção
+        end    
+    })
 
--- Raids
-local RaidsTab = Window:MakeTab({
-    Name = "Raids",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+    -- SubFarming
+    local SubFarmingTab = Window:MakeTab({
+        Name = "SubFarming",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 
--- ESP
-local ESPTab = Window:MakeTab({
-    Name = "ESP",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+    -- Stats
+    local StatsTab = Window:MakeTab({
+        Name = "Stats",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 
--- Configurações
-local ConfigTab = Window:MakeTab({
-    Name = "Configurações",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+    -- Frutas
+    local FrutasTab = Window:MakeTab({
+        Name = "Frutas",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 
--- Webhook Teleport
-local WebhookTeleportTab = Window:MakeTab({
-    Name = "Webhook Teleport",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+    -- Raids
+    local RaidsTab = Window:MakeTab({
+        Name = "Raids",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
 
----Close UI
+    -- ESP
+    local ESPTab = Window:MakeTab({
+        Name = "ESP",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    -- Configurações
+    local ConfigTab = Window:MakeTab({
+        Name = "Configurações",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    -- Webhook Teleport
+    local WebhookTeleportTab = Window:MakeTab({
+        Name = "Webhook Teleport",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+end
+
+createWindow()
+
+-- Close UI
 local ScreenGui = Instance.new("ScreenGui")
 local ImageButton = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
@@ -86,10 +146,19 @@ ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable = true
-ImageButton.Image = "http://www.roblox.com/asset/?id=113806187616600"
-ImageButton.MouseButton1Down:connect(function()
-    game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.End,false,game)
-end)
+ImageButton.Image = "http://www.roblox.com/asset/?id=1138061876" -- Verifique o ID da imagem
 
+UICorner.CornerRadius = UDim.new(0, 10) -- Define as bordas quadradas com pontas arredondadas
+UICorner.Parent = ImageButton
+
+ImageButton.MouseButton1Down:connect(function()
+    if Window then
+        Window:Destroy()
+        Window = nil
+    else
+        createWindow()
+        OrionLib:Init()
+    end
+end)
 
 OrionLib:Init()
